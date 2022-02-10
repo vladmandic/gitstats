86219b5eba7a7b76d949e34aa08142900bd707b9
Vladimir Mandic <mandic00@live.com>
2022-02-10 11:43:27 -0500

    create

---
+++ github.js
@@ -0,0 +1,54 @@
+const superagent = require('superagent');
+const log = require('@vladmandic/pilogger');
+
+const config = require('./config.json');
+
+async function github() {
+  const http = async (url) => {
+    let json = {};
+    try {
+      const res = await superagent
+        .get(url)
+        .set('content-type', 'application/json')
+        .set('accept', 'application/vnd.github.v3+json')
+        .set('user-agent', 'nodejs superagent/7.1.1')
+        .set('authorization', `token ${config.github.token}`);
+      json = JSON.parse(res.text);
+    } catch (err) {
+      log.error({ err });
+    }
+    return (Array.isArray(json)) ? { ...json } : json;
+  };
+
+  const listObjects = await http(`https://api.github.com/user/repos`);
+  const list = Object.values(listObjects).filter((r) => r.permissions.admin);
+  const size = list.reduce((prev, curr) => prev += curr.size, 0);
+  log.data({ user: config.github.user, repositories: list.length, size });
+  const repos = [];
+  for (const r of list) {
+    if (!r.permissions.admin) continue
+    repos.push({
+      name: r.full_name,
+      public: r.visibility === 'public' ? true : false,
+      created: new Date(r.created_at),
+      updated: new Date(r.updated_at),
+      size: r.size,
+      stars: r.stargazers_count,
+      forks: r.forks_count,
+      issues: r.open_issues_count,
+    });
+  }
+  return repos;
+}
+
+async function main() {
+  const repos = await github();
+  log.data('all', repos);
+  log.data('with issues', repos.filter((r) => r.issues > 0));
+  log.data('most stars', repos.sort((a, b) => b.stars - a.stars).slice(0, 3));
+  log.data('most forks', repos.sort((a, b) => b.forks - a.forks).slice(0, 3));
+  log.data('last updated', repos.sort((a, b) => b.updated - a.updated).slice(0, 3));
+  log.data('largest', repos.sort((a, b) => b.size - a.size).slice(0, 3));
+}
+
+main();
